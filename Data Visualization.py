7ef59ee8c7aa7d5c2b716cf7cc748e08c813cbbf
CommData <email.shivam.hmr@gmail.com>
2022-12-27 13:01:40 +0530

    Project Complete

--- Data Visualization.py
+++ Data Visualization.py
@@ -2,22 +2,54 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 import pickle
 import warnings
+import streamlit as st
 
+# ignore warnings
 warnings.filterwarnings('ignore')
 
+# Loading Data files divided into test and train
 X_test = pickle.load(open('data/X_test', 'rb'))
 X_train = pickle.load(open('data/X_train', 'rb'))
 
+# standardizing the data for faster and accurate result
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
+# Loading the Trained model
 model = pickle.load(open('TrainedModel.sav', 'rb'))
-# input_data = (0, 69, 179.0, 79.0, 5.0, 88.0, 38.7)
-input_data = (1, 27, 154.0, 58.0, 10.0, 81.0, 39.8)
-data_array = np.asarray(input_data)
-data_reshaped = data_array.reshape(1, -1)
-data_transformed = scaler.transform(data_reshaped)
-print(data_transformed)
-prediction_final = model.predict(data_transformed)
-print(prediction_final)
+def calories_prediction(input_data):
+    data_array = np.asarray(input_data)
+    data_reshaped = data_array.reshape(1, -1)
+    data_transformed = scaler.transform(data_reshaped)
+    prediction = model.predict(data_transformed)
+    return prediction
+
+
+st.title('Calories Burnt Prediction using AI')
+st.sidebar.image('usrImage/hmrlogo.png')
+st.sidebar.write('# Industrial Training Project')
+st.sidebar.write('# Shivam Singh\n # 07313302720\n # CSE-5A\n')
+gender = st.radio(
+        "What is your gender",
+        ('Male', 'Female')
+)
+if gender == 'Male':
+    gender = 0
+else:
+    gender = 1
+age = st.text_input('Write your age')
+height = st.text_input('Write your height in cm')
+weight = st.text_input('Write your weight in Kg')
+duration = st.text_input('Duration of exercise')
+heart_rate = st.text_input('Average heart rate')
+body_temp = st.text_input('Average body temp in celsius')
+
+
+calculated_calories = ''
+
+# creating the result button
+if st.button('Calculate the calories burnt during exercise'):
+    calculated_calories = calories_prediction([gender, age, height, weight, duration, heart_rate, body_temp])
+
+st.success(calculated_calories)
